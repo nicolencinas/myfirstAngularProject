6e8da2ab236722ae6cf9f544f96da07e9d34edc8
nicolencinas <nicolencinas@hotmail.com>
2020-09-29 19:27:49 -0300

    agregando bootstrap, fontawesome y material al projecto base

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,11 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { WatchComponent } from './watch/watch.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import {MatButtonModule} from '@angular/material/button';
+
+
 
 @NgModule({
   declarations: [
@@ -12,7 +17,9 @@ import { WatchComponent } from './watch/watch.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
